7e056bc5db6c8db657c61ed76f5d2c2e8cb6dd0d
Matias Ignacio Ruiz <mruiz@rmcuantica.com>
2022-11-22 20:46:14 -0300

    "creando componentes"

---
+++ src/components/welcome.js
@@ -0,0 +1,17 @@
+// Para indicar que es un componente react
+// Dependencias
+import React from "react"
+
+// Logica
+var gente = 'Hola Gente'
+
+// Render
+function Welcome(){
+
+    return (
+        <p>{gente}, como andas?</p>
+    )
+}
+
+// SALIDA
+export default Welcome;
